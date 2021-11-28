fe6a2e9e0faf1f1b20d0443962d4300b147bb7fe
John Joyce <15369340+john-joyce@users.noreply.github.com>
2021-11-28 12:24:47 +0000

    Added deal of day image source from file as example until api is implemented

--- CheapShark Project/csApiApp-master/csApiApp/csApiApp/Mvvm/Vm/HomePageVm.cs
+++ CheapShark Project/csApiApp-master/csApiApp/csApiApp/Mvvm/Vm/HomePageVm.cs
@@ -7,13 +7,27 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace csApiApp.Mvvm.Vm
 {
     public class HomePageVm : BaseVm
     {
+        private ImageSource _dodImage;
+
+        public ImageSource DodImage
+        {
+            get { return _dodImage; }
+            set
+            {
+                _dodImage = value;
+                SetProperty(ref _dodImage, value);
+            }
+        }
+
         public HomePageVm()
         {
+            DodImage = ImageSource.FromResource("csApiApp.Images.test2.png");
         }
     }
 }
