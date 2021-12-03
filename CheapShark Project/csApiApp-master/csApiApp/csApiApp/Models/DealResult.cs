df4c2e5092f6fc43fb728262cec3417201e5c299
John Joyce <15369340+john-joyce@users.noreply.github.com>
2021-12-03 10:11:12 +0000

    Seperated deals from games

---
+++ CheapShark Project/csApiApp-master/csApiApp/csApiApp/Models/DealResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace csApiApp.Models
+{
+    public class DealResult : GameResultClass
+    {
+        [JsonProperty("normalPrice")]
+        public float NormalPrice { get; set; }
+
+        [JsonProperty("salePrice")]
+        public float SalePrice { get; set; }
+    }
+}
