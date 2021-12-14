34abb822240a007b978422b618633e9c08683153
John Joyce <15369340+john-joyce@users.noreply.github.com>
2021-12-14 06:42:18 +0000

    Updated DOD endpoint

---
+++ CheapShark Project/csApiApp-master/csApiApp/csApiApp/Services/Rest/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace csApiApp.Services.Rest.Endpoints
+{
+    public static class Constants
+    {
+        public const string DealOfTheDayEndpoint = "https://www.cheapshark.com/api/1.0/deals?sortBy=Savings&pageSize=1&lowerPrice=0.0001";
+
+        public const string searchStartPoint = "https://www.cheapshark.com/api/1.0/games?title=";
+        public const string searchEndPoint = "&limit=60&exact=0";
+    }
+}
