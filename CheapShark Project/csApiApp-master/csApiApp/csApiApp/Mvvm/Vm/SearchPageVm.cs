247966b158b5e2c4bafb8b2ccb958af8896edec8
John Joyce <15369340+john-joyce@users.noreply.github.com>
2021-12-16 07:43:33 +0000

    Added ability to search from within the search page

--- CheapShark Project/csApiApp-master/csApiApp/csApiApp/Mvvm/Vm/SearchPageVm.cs
+++ CheapShark Project/csApiApp-master/csApiApp/csApiApp/Mvvm/Vm/SearchPageVm.cs
@@ -1,16 +1,22 @@
 ﻿using csApiApp.Mvvm.Model;
 using csApiApp.Services.Rest;
 using csApiApp.Services.Rest.Endpoints;
+using FunctionZero.CommandZero;
+using FunctionZero.MvvmZero;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace csApiApp.Mvvm.Vm
 {
     internal class SearchPageVm : BaseVm
     {
         private CheapSharkAPI _cheapSharkAPI;
+
+        public ICommand SearchCommand { get; }
 
         private ObservableCollection<SearchResult> _searchResults;
 
@@ -20,14 +26,30 @@ namespace csApiApp.Mvvm.Vm
             set => base.SetProperty(ref _searchResults, value);
         }
 
+        private string _searchText;
+
+        public string SearchText
+        {
+            get => _searchText;
+            set => base.SetProperty(ref _searchText, value);
+        }
+
         public SearchPageVm(CheapSharkAPI cheapSharkAPI)
         {
             _cheapSharkAPI = cheapSharkAPI;
+
+            SearchCommand = new CommandBuilder().AddGuard(this).SetExecuteAsync(SearchGames).SetName("Search").Build();
         }
 
         internal async void Init(string text)
         {
-            string url = Constants.searchStartPoint + text + Constants.searchEndPoint;
+            SearchText = text;
+            await SearchGames();
+        }
+
+        internal async Task SearchGames()
+        {
+            string url = Constants.searchStartPoint + SearchText + Constants.searchEndPoint;
             var results = await _cheapSharkAPI.GetSearchAsync(url);
             SearchResults = new ObservableCollection<SearchResult>(results);
         }
