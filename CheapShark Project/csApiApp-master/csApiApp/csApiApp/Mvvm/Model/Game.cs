f17623fc55bf24b17dee6294ca88d49f6a71366c
John Joyce <15369340+john-joyce@users.noreply.github.com>
2022-02-01 17:06:22 +0000

    Updated SimpleInjector, Xamarin.Forms, Xamarin.Essentials to latest

---
+++ CheapShark Project/csApiApp-master/csApiApp/csApiApp/Mvvm/Model/Game.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using SQLite;
+
+namespace csApiApp.Models
+{
+    public class Game
+    {
+        [PrimaryKey]
+        [JsonProperty("gameID")]
+        public int GameID { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("metacriticlink")]
+        public string MetaReviewLink { get; set; }
+
+        [JsonProperty("metacriticScore")]
+        public int MetaReviewScore { get; set; }
+
+        [JsonProperty("steamRatingPercent")]
+        public float SteamRatingPercent { get; set; }
+
+        [JsonProperty("steamAppID", NullValueHandling = NullValueHandling.Ignore)]
+        public string SteamAppID { get; set; }
+
+        [JsonProperty("releaseDate")]
+        public int ReleaseDate { get; set; }
+
+        [JsonProperty("thumb")]
+        public string ThumbnailLink { get; set; }
+    }
+}
