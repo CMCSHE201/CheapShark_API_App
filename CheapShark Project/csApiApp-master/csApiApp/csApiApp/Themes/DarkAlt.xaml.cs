1244f5902ec17e86e80c59023ce1c743a6d5806e
Mick-Bee <93978004+Mick-Bee@users.noreply.github.com>
2022-02-02 12:40:20 +0000

    Themes (#42)

---
+++ CheapShark Project/csApiApp-master/csApiApp/csApiApp/Themes/DarkAlt.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace csApiApp.Themes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DarkAlt : ResourceDictionary
+    {
+        public DarkAlt()
+        {
+            InitializeComponent();
+        }
+    }
+}
